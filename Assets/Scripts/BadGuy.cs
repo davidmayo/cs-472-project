6f31914dfe1357b13ae01dff82cfd90d98a53f59
David Mayo <dcmayo@gmail.com>
2022-12-01 21:41:10 -0500

    Working on pathfinding

--- Assets/Scripts/BadGuy.cs
+++ Assets/Scripts/BadGuy.cs
@@ -9,14 +9,18 @@ public class BadGuy : MonoBehaviour
 {
     Reset Lscript;
     //private GameObject camera;
-    public Transform Player;
-    public NavMeshAgent nav;
+    public Transform playerTransform;
+    public NavMeshAgent navMeshAgent;
+
+    public TerrainCollider terrainCollider;
+    public LayerMask terrainMask;
 
     // Start is called before the first frame update
     void Start()
-    { //This code shows the bad guy that it will follow the player
-        Player = GameObject.FindGameObjectWithTag("Player").transform;
-        nav = GetComponent<NavMeshAgent>();
+    {
+        //This code shows the bad guy that it will follow the player
+        //playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        //navMeshAgent = GetComponent<NavMeshAgent>();
 
         //Lscript = camera.GetComponent<Reset>();
         Lscript = FindObjectOfType<Reset>();
@@ -26,7 +30,51 @@ public class BadGuy : MonoBehaviour
     void Update()
     {
         //This is so the bad guy is always update on the players location
-        //nav.SetDestination(Player.position);
+        if (navMeshAgent.isOnNavMesh)
+        {
+            //Debug.Log($"Bad Guy IS on NavMesh pos={this.transform.position}");
+
+            navMeshAgent.destination = playerTransform.position;
+            Debug.Log($"Bad Guy IS on NavMesh pos={this.transform.position}\tSetting target to {playerTransform.position}\tdest={navMeshAgent.destination}");
+        }
+        else
+        {
+            string message = $"NOT ON NAV MESH";
+
+            // Get onto the nav mesh, somehow.
+            NavMeshTriangulation navMeshTriangulation = NavMesh.CalculateTriangulation();
+            message += $"\tnavMeshTriangulation = {navMeshTriangulation} Length={navMeshTriangulation.vertices.Length}";
+            int index = Random.Range(0, navMeshTriangulation.vertices.Length);
+            NavMeshHit hit;
+
+            if(NavMesh.SamplePosition(navMeshTriangulation.vertices[index], out hit, 20000f, NavMesh.AllAreas))
+            {
+                message += $"\tTELEPORTING to {hit.position}";
+                navMeshAgent.Warp(hit.position);
+            }
+            else
+            {
+                message += $"\tSamplePosition() was false. hit={hit}";
+            }
+            Debug.Log(message);
+
+            //// Manually force the y value to collide with the terrain
+            //RaycastHit raycastHit;
+
+            //string message = $"WARNING: Bad Guy NOT on NavMesh pos={this.transform.position}";
+
+            //if (Physics.Raycast(transform.position, Vector3.down, out raycastHit, maxDistance: 100f, layerMask: terrainMask) ||
+            //    Physics.Raycast(transform.position, Vector3.up,   out raycastHit, maxDistance: 100f, layerMask: terrainMask))
+            //{
+
+            //    Debug.Log($"{message}\nMoving to {raycastHit.point}");
+            //    this.transform.position = raycastHit.point;
+            //}
+            //else
+            //{
+            //    Debug.Log($"{message}\nERROR: Cannot find position on terrain");
+            //}
+        }
     }
 
     private void OnCollisionEnter(Collision col)
